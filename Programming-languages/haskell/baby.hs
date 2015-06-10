81e630693be432c3712e66d4149beab3843e7584
zhangmh <zhangmhao@gmail.com>
2015-06-10 15:13:25 +0800

    使用递归重新实现一遍一些基础函数

--- Programming-languages/haskell/baby.hs
+++ Programming-languages/haskell/baby.hs
@@ -104,7 +104,40 @@ maximum' (x:xs)
     where maxTail = maximum' xs  
 
 -- 使用max来简化 maximum 的逻辑
-maximum' :: (Ord a) => [a] -> a
-maximum' [] = error "maximum of empty list"
-maximum' [x] = x
-maximum' (x:xs) = max x (maximum' xs)
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum' xs)
+
+-- replicate
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+--take
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0   = []
+take' _ []     = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+
+
+elem'' :: (Eq a) => a -> [a] -> Bool
+elem'' _ [] = False
+elem'' a (x:xs)
+    | a == x = True
+    | otherwise = a `elem''` xs
