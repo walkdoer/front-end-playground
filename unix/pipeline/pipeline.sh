626c65f34151d19e370486350ca41c3625b6b9ef
zhangmh <zhangmhao@gmail.com>
2015-09-21 18:19:51 +0800

    calculate position

--- unix/pipeline/pipeline.sh
+++ unix/pipeline/pipeline.sh
@@ -3,3 +3,6 @@ awk '{print $8}' data/access-1.log | # Split by whitespace, 7th field is request
     uniq -c                 | # Replace consecutive occurrences of the same URL with a count
     sort -rn                | # Sort by number of occurrences, descending
     head -n 5                 # Output top 5 URLs
+
+
+# 命令行的性能非常优秀 http://aadrake.com/command-line-tools-can-be-235x-faster-than-your-hadoop-cluster.html
